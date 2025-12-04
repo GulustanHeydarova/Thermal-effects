eac07d95b6ad7fba4009d897022239d290473677
Gulustan Heydarova <heydrovagulustan@gmail.com>
2025-12-04 15:51:12 +0400

    Update and rename 40-2.py to Multiple steady states-2/40-2.py

--- Multiple steady states-2/40-2.py
+++ Multiple steady states-2/40-2.py
@@ -1,7 +1,4 @@
-# File 40-2.py
-# Example 40. Multiple steady states
-# Copyright (C) 2016, Szczepan Bednarz
-# Released under the GNU General Public License
+
 
 import matplotlib.pyplot as plt
 import numpy as np
